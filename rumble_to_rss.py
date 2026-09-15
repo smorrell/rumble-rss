fb873f7e006c6ecfe2b640a380f37d5ac55021ea
smorrell <smorrell@rogers.com>
2026-09-14 22:40:23 -0400

    Update Rumble to RSS pipeline and Scott Adams extraction

--- rumble_to_rss.py
+++ rumble_to_rss.py
@@ -188,7 +188,7 @@ def download_and_convert():
             {
                 "key": "FFmpegExtractAudio",
                 "preferredcodec": "mp3",
-                "preferredquality": "16",
+                "preferredquality": "128",
             }
         ],
         "outtmpl": os.path.join(
