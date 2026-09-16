4ae3756da229a733f6135d8f5aca7daf790127c5
smorrell <smorrell@rogers.com>
2026-09-16 07:33:16 -0400

    Use GitHub URLs for feeds and rename batch file
    Update AnnCoulter.xml to use GitHub-hosted enclosure URLs and normalize XML formatting. Change default FEED_BASE_URL in rumble_to_rss.py from http://localhost:3000 to the repository GitHub blob URL so generated feeds point at the repo-hosted MP3s. Rename AndAndNick.feed.bat to Podcast.feeds.bat (file rename only). These changes make feed resources publicly accessible instead of linking to localhost.

--- rumble_to_rss.py
+++ rumble_to_rss.py
@@ -30,7 +30,9 @@ AUDIO_DIR = os.path.join(REPO_PATH, "mp3s")
 METADATA_PATH = os.path.join(AUDIO_DIR, "video_metadata.json")
 ANN_COULTER_FEED_PATH = os.path.join(REPO_PATH, "AnnCoulter.xml")
 ANN_COULTER_CHANNEL_URL = "https://rumble.com/c/AnnCoulter"
-FEED_BASE_URL = os.environ.get("FEED_BASE_URL", "http://localhost:3000").rstrip("/")
+FEED_BASE_URL = os.environ.get(
+    "FEED_BASE_URL", "https://github.com/smorrell/rumble-rss/blob/master"
+).rstrip("/")
 PODCAST_COVER_URL = (
     "https://raw.githubusercontent.com/smorrell/rumble-rss/master/podcast_cover.jpg"
 )
